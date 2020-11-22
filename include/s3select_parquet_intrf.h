9f16d4c88673c61bde8d1aec85af8ee4c04e4bce
gal salomon <gal.salomon@gmail.com>
2020-11-22 23:08:23 +0200

    replace the interface with arrow; (1) on previous version it load all table, now it retrieve only the column (2) the scan is row-group after row-group.  on later phase the implementation layer (access to file) will replace with ceph-access-layer.

--- include/s3select_parquet_intrf.h
+++ include/s3select_parquet_intrf.h
@@ -8,111 +8,149 @@
 #include <parquet/arrow/writer.h>
 #include <parquet/exception.h>
 #include <set>
+#include <parquet/column_reader.h>
 
 /******************************************/
-class parquet_file_parser {
- 
- public:
-  enum class parquet_type {STRING,INT32,INT64,DOUBLE};
-  typedef std::vector<std::pair<std::string,parquet_type>> schema_t;
- private:  
- 
-  std::string parquet_file_name;
-  uint32_t m_num_of_columms;
-  uint64_t m_num_of_rows;
-  uint64_t m_rownum;
-  uint64_t m_chunk_num;
-  schema_t m_schm;
-  std::shared_ptr<arrow::Table> m_table;
-  std::shared_ptr<arrow::io::ReadableFile> m_infile;
-  std::unique_ptr<parquet::arrow::FileReader> m_reader;
+class column_reader_wrap
+{
 
-  int load_meta_data() {
-    PARQUET_ASSIGN_OR_THROW(m_infile, arrow::io::ReadableFile::Open(
-                                        parquet_file_name, arrow::default_memory_pool()));
+private:
 
-    PARQUET_THROW_NOT_OK(
-        parquet::arrow::OpenFile(m_infile, arrow::default_memory_pool(), &m_reader));
+  int64_t m_rownum;
+  parquet::Type::type m_type;
+  std::shared_ptr<parquet::RowGroupReader> m_row_group_reader;
+  uint32_t m_row_grouop_id;
+  uint16_t m_col_id;
+  parquet::ParquetFileReader* m_parquet_reader;
+  std::shared_ptr<parquet::ColumnReader> m_ColumnReader;
+  bool m_end_of_stream;
+  bool m_read_last_value;
+  
 
-    m_reader->set_use_threads(false);
+public:
 
-    std::shared_ptr<::arrow::Schema> scm;
-    m_reader->GetSchema(&scm);
+  enum class parquet_type
+  {
+    STRING,
+    INT32,
+    INT64,
+    DOUBLE
+  };
 
-    m_num_of_columms = scm.get()->fields().size();
-
-    for (auto x : scm.get()->fields()) {
-      //parquet schema is uploaded as pairs <column-name,data-type>
-      if(x.get()->type().get()->ToString().compare("string")==0)
-      {
-          std::pair<std::string, parquet_type> elm(x.get()->name(),parquet_type::STRING);
-          m_schm.push_back(elm);
-      }
-      else if(x.get()->type().get()->ToString().compare("int32")==0)
-      {
-          std::pair<std::string, parquet_type> elm(x.get()->name(),parquet_type::INT32);
-          m_schm.push_back(elm);  
-      }
-      else if(x.get()->type().get()->ToString().compare("int64")==0)
-      {
-          std::pair<std::string, parquet_type> elm(x.get()->name(),parquet_type::INT64);
-          m_schm.push_back(elm);  
-      }
-      else if(x.get()->type().get()->ToString().compare("double")==0)
-      {
-          std::pair<std::string, parquet_type> elm(x.get()->name(),parquet_type::DOUBLE);
-          m_schm.push_back(elm);
-      }
-      else
-      {
-        return -1;//TODO throw exception
-      }
-    }
-
-    return 0;
-  }
-
- public:
-
-  typedef std::set<uint16_t> column_pos_t;
-   
   typedef struct
   {
     int64_t num;
-    char* str;//str is pointing to offset in string which is NOT null terminated.
+    char *str; //str is pointing to offset in string which is NOT null terminated.
     uint16_t str_len;
     double dbl;
     parquet_type type;
   } parquet_value_t;
 
-  typedef std::vector<parquet_value_t> row_values_t;
+  private:
+  parquet_value_t m_last_value;
 
-  parquet_file_parser(std::string name) : 
-  parquet_file_name(name),
-  m_num_of_columms(0),
-  m_num_of_rows(0),
-  m_rownum(0),
-  m_chunk_num(0) 
-  {load_meta_data();}
+  public:
+  column_reader_wrap(std::unique_ptr<parquet::ParquetFileReader> & parquet_reader,uint16_t col_id);
+
+  parquet::Type::type get_type();
+
+  bool HasNext();//TODO template 
+
+  int64_t ReadBatch(int64_t batch_size, int16_t* def_levels, int16_t* rep_levels,
+                            parquet_value_t* values, int64_t* values_read);
+
+  int Read(uint64_t rownum,parquet_value_t & value);
+
+};
+
+class parquet_file_parser
+{
+
+public:
+
+  typedef std::vector<std::pair<std::string, column_reader_wrap::parquet_type>> schema_t;
+  typedef std::set<uint16_t> column_pos_t;
+  typedef std::vector<column_reader_wrap::parquet_value_t> row_values_t;
+
+  typedef column_reader_wrap::parquet_value_t parquet_value_t;
+  typedef column_reader_wrap::parquet_type parquet_type;
+
+private:
+
+  std::string m_parquet_file_name;
+  uint32_t m_num_of_columms;
+  uint64_t m_num_of_rows;
+  uint64_t m_rownum;
+  schema_t m_schm;
+  int m_num_row_groups;
+  std::shared_ptr<parquet::FileMetaData> m_file_metadata;
+  std::unique_ptr<parquet::ParquetFileReader> m_parquet_reader;
+  std::vector<column_reader_wrap*> m_column_readers;
+
+  public:
+
+  parquet_file_parser(std::string parquet_file_name) : 
+                                   m_parquet_file_name(parquet_file_name),
+                                   m_num_of_columms(0),
+                                   m_num_of_rows(0),
+                                   m_rownum(0),
+                                   m_num_row_groups(0)
+                                   
+                                   
+  {
+    load_meta_data();
+  }
+
+  int load_meta_data()
+  {
+    m_parquet_reader = parquet::ParquetFileReader::OpenFile(m_parquet_file_name, false);
+    m_file_metadata = m_parquet_reader->metadata();
+    m_num_of_columms = m_parquet_reader->metadata()->num_columns();
+    m_num_row_groups = m_file_metadata->num_row_groups();
+    m_num_of_rows = m_file_metadata->num_rows();
+
+    for (int i = 0; i < m_num_of_columms; i++)
+    {
+      parquet::Type::type tp = m_file_metadata->schema()->Column(i)->physical_type();
+      std::pair<std::string, column_reader_wrap::parquet_type> elm;
+
+      switch (tp)
+      {
+      case parquet::Type::type::INT32:
+        elm = std::pair<std::string, column_reader_wrap::parquet_type>(m_file_metadata->schema()->Column(i)->name(), column_reader_wrap::parquet_type::INT32);
+        m_schm.push_back(elm);
+        break;
+
+      case parquet::Type::type::INT64:
+        elm = std::pair<std::string, column_reader_wrap::parquet_type>(m_file_metadata->schema()->Column(i)->name(), column_reader_wrap::parquet_type::INT64);
+        m_schm.push_back(elm);
+        break;
+
+      case parquet::Type::type::DOUBLE:
+        elm = std::pair<std::string, column_reader_wrap::parquet_type>(m_file_metadata->schema()->Column(i)->name(), column_reader_wrap::parquet_type::DOUBLE);
+        m_schm.push_back(elm);
+        break;
+
+      case parquet::Type::type::BYTE_ARRAY:
+        elm = std::pair<std::string, column_reader_wrap::parquet_type>(m_file_metadata->schema()->Column(i)->name(), column_reader_wrap::parquet_type::STRING);
+        m_schm.push_back(elm);
+        break;
+
+      default:
+        //throw base_s3select_exception("some parquet type not supported",s3selectEngine::base_s3select_exception::s3select_exp_en_t::FATAL);//TODO better message
+        return -1;
+      }
+
+      m_column_readers.push_back(new column_reader_wrap(m_parquet_reader,i));
+    }
+
+    return 0;
+  }
 
   bool end_of_stream()
   {
-    if(m_table.get() == 0)
-    {
-      PARQUET_THROW_NOT_OK(m_reader->ReadTable(&m_table));
-      m_num_of_rows = m_table->num_rows();
-    }
 
-    if(m_rownum >= m_num_of_rows)
-    {//next chunk if exists
-      if(m_table->column(0)->num_chunks()<m_chunk_num)
-      {
-        m_chunk_num++;
-        m_rownum = m_table->column(0)->chunk(m_chunk_num)->length();
-      }
-    }
-
-    if(m_rownum >= m_num_of_rows)
+    if (m_rownum >= m_num_of_rows)
       return true;
     return false;
   }
@@ -124,7 +162,7 @@ class parquet_file_parser {
 
   bool increase_rownum()
   {
-    if(end_of_stream())
+    if (end_of_stream())
       return false;
 
     m_rownum++;
@@ -143,78 +181,173 @@ class parquet_file_parser {
 
   int get_column_values_by_positions(column_pos_t positions, row_values_t &row_values)
   {
-    //per each position get type , extract and push to row_values (intensive method)
-    //TODO next chunk
-    parquet_value_t column_value;
+    column_reader_wrap::parquet_value_t column_value;
     row_values.clear();
 
-    if (end_of_stream())
-      return -1;
-
-    for (auto idx : positions)
+    for(auto col : positions)
     {
-      switch (m_schm[idx].second)
-      {
-        case parquet_type::STRING:
-        {
-          auto string_column =
-              std::static_pointer_cast<arrow::StringArray>(m_table->column(idx)->chunk(m_chunk_num));
-
-          column_value.str = (char *)string_column->raw_data() + string_column->value_offset(m_rownum);
-          column_value.str_len = string_column->value_length(m_rownum);
-          column_value.type = parquet_type::STRING;
-        }
-        break;
-
-        case parquet_type::INT64:
-        {
-          column_value.num =
-              std::static_pointer_cast<arrow::Int64Array>(m_table->column(idx)->chunk(m_chunk_num))->Value(m_rownum);
-          column_value.type = parquet_type::INT64;
-        }
-        break;
-
-        case parquet_type::INT32:
-        {
-          column_value.num =
-              std::static_pointer_cast<arrow::Int32Array>(m_table->column(idx)->chunk(m_chunk_num))->Value(m_rownum);
-          column_value.type = parquet_type::INT32;
-        }
-        break;
-
-        case parquet_type::DOUBLE:
-        {
-          column_value.dbl =
-              std::static_pointer_cast<arrow::DoubleArray>(m_table->column(idx)->chunk(m_chunk_num))->Value(m_rownum);
-          column_value.type = parquet_type::DOUBLE;
-        }
-        break;
-
-        default:
+      if((col)>=m_num_of_columms)
+      {//TODO should verified upon syntax phase 
         //TODO throw exception
         return -1;
       }
-        row_values.push_back(column_value);
+      m_column_readers[col]->Read(m_rownum,column_value);
+      row_values.push_back(column_value);//TODO intensive (should move)
     }
-
     return 0;
-  }
-
-  uint16_t get_column_id(std::string column_name) {
-    uint16_t pos = 0;
-    // search meta-data from column-id by name (done once per query)
-    for (auto x : m_table->fields()) {
-      if (x.get()->name().compare(column_name) == 0) {
-        return pos;
-      }
-      pos++;
-    }
-    return -1;
   }
 
   schema_t get_schema()
   {
     return m_schm;
   }
-
 };
+
+/******************************************/
+
+
+  column_reader_wrap::column_reader_wrap(std::unique_ptr<parquet::ParquetFileReader> & parquet_reader,uint16_t col_id):
+  m_rownum(1),
+  m_type(parquet::Type::type::UNDEFINED),
+  m_row_grouop_id(0),
+  m_col_id(col_id),
+  m_end_of_stream(false),
+  m_read_last_value(false)
+  {
+    m_parquet_reader = parquet_reader.get();
+    m_row_group_reader = m_parquet_reader->RowGroup(m_row_grouop_id);
+    m_ColumnReader = m_row_group_reader->Column(m_col_id);
+  }
+
+  parquet::Type::type column_reader_wrap::get_type()
+  {//TODO if UNDEFINED 
+    return m_parquet_reader->metadata()->schema()->Column(m_col_id)->physical_type();
+  }
+
+  bool column_reader_wrap::HasNext()//TODO template 
+  {
+    parquet::Int32Reader* int32_reader;
+    parquet::Int64Reader* int64_reader;
+    parquet::DoubleReader* double_reader;
+    parquet::ByteArrayReader* byte_array_reader;
+
+    switch (get_type())
+    {
+    case parquet::Type::type::INT32:
+      int32_reader = static_cast<parquet::Int32Reader *>(m_ColumnReader.get());
+      return int32_reader->HasNext();
+      break;
+
+    case parquet::Type::type::INT64:
+      int64_reader = static_cast<parquet::Int64Reader *>(m_ColumnReader.get());
+      return int64_reader->HasNext();
+      break;
+
+    case parquet::Type::type::DOUBLE:
+      double_reader = static_cast<parquet::DoubleReader *>(m_ColumnReader.get());
+      return double_reader->HasNext();
+      break;
+
+    case parquet::Type::type::BYTE_ARRAY:
+      byte_array_reader = static_cast<parquet::ByteArrayReader *>(m_ColumnReader.get());
+      return byte_array_reader->HasNext();
+      break;
+
+    default:
+      return false;
+      //TODO throw exception
+    }
+
+    return false;
+  }
+
+  int64_t column_reader_wrap::ReadBatch(int64_t batch_size, int16_t* def_levels, int16_t* rep_levels,
+                            parquet_value_t* values, int64_t* values_read)
+  {
+    parquet::Int32Reader* int32_reader;
+    parquet::Int64Reader* int64_reader;
+    parquet::DoubleReader* double_reader;
+    parquet::ByteArrayReader* byte_array_reader;
+
+    parquet::ByteArray str_value;
+    int64_t rows_read;
+    int32_t i32_val;
+
+
+    switch (get_type())
+    {
+    case parquet::Type::type::INT32:
+      int32_reader = static_cast<parquet::Int32Reader *>(m_ColumnReader.get());
+      rows_read = int32_reader->ReadBatch(1, nullptr, nullptr,&i32_val, values_read);
+      values->num = i32_val;
+      values->type = column_reader_wrap::parquet_type::INT32;
+      break;
+
+    case parquet::Type::type::INT64:
+      int64_reader = static_cast<parquet::Int64Reader *>(m_ColumnReader.get());
+      rows_read = int64_reader->ReadBatch(1, nullptr, nullptr, (int64_t *)&(values->num), values_read);
+      values->type = column_reader_wrap::parquet_type::INT64;
+      break;
+
+    case parquet::Type::type::DOUBLE:
+      double_reader = static_cast<parquet::DoubleReader *>(m_ColumnReader.get());
+      rows_read = double_reader->ReadBatch(1, nullptr, nullptr, (double *)&(values->dbl), values_read);
+      values->type = column_reader_wrap::parquet_type::DOUBLE;
+      break;
+
+    case parquet::Type::type::BYTE_ARRAY:
+      byte_array_reader = static_cast<parquet::ByteArrayReader *>(m_ColumnReader.get());
+      rows_read = byte_array_reader->ReadBatch(1, nullptr, nullptr, &str_value , values_read);
+      values->str = (char*)str_value.ptr;
+      values->str_len = str_value.len;
+      values->type = column_reader_wrap::parquet_type::STRING;
+      break;
+    //TODO default; exception
+    }
+
+    return rows_read;
+  }
+
+  int column_reader_wrap::Read(const uint64_t rownum,parquet_value_t & value)
+  {//m_rownum should be smaller than rownum
+
+    int64_t values_read = 0;
+    int64_t rows_read = 0;
+
+    while (m_rownum < (int64_t)rownum)
+    { //should advance
+      m_read_last_value = false;
+      if (HasNext() == false)
+      { //end of stream or next row-group
+        if ((m_row_grouop_id+1) >= m_parquet_reader->metadata()->num_row_groups())
+        {
+          m_end_of_stream = true;
+          return -2; //end-of-stream
+        }
+        else
+        {
+          m_row_grouop_id++;
+          m_row_group_reader = m_parquet_reader->RowGroup(m_row_grouop_id);
+          m_ColumnReader = m_row_group_reader->Column(m_col_id);
+        }
+      }
+      else
+      {
+        m_rownum++;
+      }
+    } //end-while
+
+    if(m_read_last_value==false) 
+    {
+      rows_read = ReadBatch(1,nullptr , nullptr,&m_last_value, &values_read);
+      m_read_last_value = true;
+    }
+    value = m_last_value;
+
+    //TODO rows_read == 1?
+    //TODO values_read == 1?
+
+    return 0;
+  }
+
+
